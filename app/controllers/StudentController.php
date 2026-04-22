a79852765f26bfeee37f4407507062a890c1ba1f
AliceeHk <alicehk1218@gmail.com>
2026-04-22 09:12:27 +0700

    wip: implement student edit and delete

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -44,6 +44,20 @@ class StudentController extends Controller
         $studentModel = new Student();
         $studentModel->insert($_POST);
     }
+
+    public function update(string $id)
+    {
+        $id = intval($id); //untuk convert string ke integer
+        $studentModel = new Student();
+        $studentModel->update($_POST, $id);
+    }
+    
+    public function destroy(string $id)
+    {
+        $id = intval($id); //untuk convert string ke integer
+        $studentModel = new Student();
+        $studentModel->delete($id);
+    }
 }
 
 ?>
--- public/index.php
+++ public/index.php
@@ -12,6 +12,8 @@ $router->add('GET', '/students/{id}', 'StudentController', 'show');
 $router->add('GET', '/students/{id}/edit', 'StudentController', 'edit');
 
 $router->add('POST', '/students','StudentController','store');
+$router->add('PUT', '/students/{id}', 'StudentController', 'update'); //bisa pakai  PUT (ubah 1 tabel) ataupun PATCH (pas ubah 1 kolom)
+$router->add('DELETE', '/students/{id}', 'StudentController', 'destroy');
 
 $router->run();
 ?>
